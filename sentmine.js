da16b33b9febfb29afd39905020f72f36acc414d
Bryan Jennings <bryanjenningz@gmail.com>
2016-07-08 03:30:58 -0700

    Update sentmine.js

--- sentmine.js
+++ sentmine.js
@@ -2,64 +2,72 @@ var request = require('request');
 var fs = require('fs');
 
 var file = 'sentences.txt';
-var rootUrl = 'http://www.chinasmack.com/page/';
-var maxPageNumber = 225;
+var rootUrl = 'http://www.japancrush.com/page/';
+var maxPageNumber = 80;
 var linkRegex = /<a id="[^"]+" class="[^"]+" href="([^"]+)"/g;
 var sentenceRegex = /<span title="([^"]+)">([^"]+)<\/span>/g;
 
 fs.writeFile(file, ''); // Clear file
 
-var filterTags = function(text) {
-  return text.replace(/<[^>]+>[^<]*<[^>]+>/g, '')
-             .replace(/<[^>]+>/g, '')
-             .replace(/&[^;]+;/g, '\'')
+var cleanText = function(text) {
+  return text.replace(/<[^>]+>/g, '')
+             .replace(/&[^;]+;/g, '')
              .replace(/[,\n]/g, '');
 };
 
-// Input: row (tuple [Japanese, English])
-var writeRow = function(row) {
-  return row.map(filterTags).join(',') + '\n';
-};
-
 var writeRows = function(rows, file) {
-  var text = '';
-  for (var i = 0; i < rows.length; i++) {
-    var row = rows[i];
-    var entry = writeRow(row);
-    if (entry.indexOf('\n,') >= 0) {
-      console.log(entry);
-    }
-    text += entry;
+  var textRows = ''.concat.apply('', 
+    rows.map((row) => row.map(cleanText).join(',') + '\n'));
+  if (/,,/.test(textRows)) {
+    console.log('2 commas!!! ------------------------------------------');
+    var index = textRows.indexOf(',,');
+    var startIndex = textRows.lastIndexOf('\n', index);
+    var endIndex = textRows.indexOf('\n', index);
+    console.log(textRows.slice(startIndex, endIndex + 1))
+    console.log(' ----------------------------------------- ')
   }
-  fs.appendFile(file, text);
-  return text;
+  fs.appendFile(file, textRows);
 };
 
 var collectMatches = function(regex, string, extract) {
-  extract = extract || function(match) { return match[1]; };
-  var match;
-  var matches = [];
-  while (match = regex.exec(string)) {
-    matches.push(extract(match));
+  extract = extract || ((match) => match[1]);
+  var matches = string.match(regex);
+  if (matches === null) {
+    console.log('Found nothing for regex: ' + regex);
+    return [];
   }
-  return matches;
+  return matches.map((link) => {
+    regex.lastIndex = 0;
+    return extract(regex.exec(link));
+  });
 };
 
 var collectSentences = function(pageNumber) {
   console.log(pageNumber);
   request(rootUrl + pageNumber, function(error, response, body) {
+    if (error) {
+      console.log('Error line 43: ' + error);
+      return;
+    }
     var links = collectMatches(linkRegex, body);
     for (var i = 0; i < links.length; i++) {
       var link = links[i];
-      request(link, function(error, response, articleBody) {
-        var getSentences = function(match) { return match.slice(1, 3); };
-        var sentences = collectMatches(sentenceRegex, articleBody, getSentences);
-        writeRows(sentences, file);
-      });
+      (function(link) {
+        request(link, function(error, response, articleBody) {
+          // console.log(link);
+          if (error) {
+            console.log('Error line 51: ' + error);
+            return;
+          }
+          var extractInfo = (match) => match.slice(1, 3).concat(link);
+          var sentences = collectMatches(sentenceRegex, articleBody, extractInfo);
+          writeRows(sentences, file);
+        });
+      })(link);
     }
   });
 };
 
 for (var pageNumber = 1; pageNumber <= maxPageNumber; pageNumber++) {
-  setTimeout(collectSentences.bind(null, pageNumber), 10000 * pageNumber);
+  setTimeout(collectSentences.bind(null, pageNumber), 20000 * (pageNumber - 1));
 }
